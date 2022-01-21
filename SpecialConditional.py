d8a5b484b3f94baa5bf175d016822838cdaf7102
Dineshkumar <dineshkumar.erd@gmail.com>
2022-01-21 11:53:04 -0700

    Create SpecialConditional.py

---
+++ SpecialConditional.py
@@ -0,0 +1,8 @@
+#Given two boolean variables a and b, one of the following statements works differently than the others. Which one is it?
+
+not (a == b)
+a == (not b)
+a == not b
+not a == b
+
+# Answer is: a == not b (it will throw invalid syntax error)
